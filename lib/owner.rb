34746d35625b4b3e49b6b97e274b508ac8a97b22
Tony Rim <duc_thinh_03@yahoo.com>
2019-12-05 02:03:05 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,87 @@
+# require "pry"
 class Owner
-  # code goes here
+ 
+  attr_reader :name, :species
+ 
+
+@@all = []
+
+
+  def initialize(name)
+    @name = name 
+    @species = "human"
+    @@all << self 
+  end
+
+
+  def say_species 
+    "I am a human."
+  end 
+
+
+  def self.all
+    @@all
+  end 
+
+
+  def self.count 
+    @@all.count
+  end 
+
+
+  def self.reset_all
+    @@all.clear
+  end 
+
+
+  def cats
+    Cat.all.select {|x| x.owner == self}
+  end 
+
+
+  def dogs
+    Dog.all.select {|x| x.owner == self}
+  end 
+
+
+  def buy_cat(name)
+    Cat.new(name, self)
+  end 
+
+
+  def buy_dog(name)
+    Dog.new(name, self)
+  end 
+
+  def walk_dogs
+    dogs.each {|dog| dog.mood = 'happy'}
+  end 
+
+  def feed_cats
+    cats.each {|cat| cat.mood = "happy"}
+  end 
+
+
+
+  def sell_pets 
+    dogs.each do |pet|
+        pet.mood = "nervous"
+        pet.owner = nil
+    end 
+    cats.each do |pet|
+        pet.mood = "nervous"
+        pet.owner = nil
+    end 
+  end 
+
+
+
+  def list_pets 
+     "I have #{cats.size} dog(s), and #{dogs.size} cat(s)."
+  end 
+
+
+
+
+
 end
