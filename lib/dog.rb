e2735c03572fa0dbe2e1e7cd0ddb5ad57739450c
Tony Rim <duc_thinh_03@yahoo.com>
2019-12-05 02:02:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
-  # code goes here
+  
+  attr_reader :name 
+  attr_accessor :owner, :mood
+
+  CONDOGS = []
+
+  def initialize(name, owner)
+    @name = name 
+    @owner = owner
+    @mood = "nervous"
+    CONDOGS << self 
+  end 
+
+  def self.all
+    CONDOGS
+  end 
+
 end
